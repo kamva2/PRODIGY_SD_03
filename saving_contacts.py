a627022f38c69ef41d8cf1877b9b17e7f26f4deb
Kamva Poswa <167709620+kamva2@users.noreply.github.com>
2024-07-01 12:21:37 +0200

    Update saving_contacts.py

--- saving_contacts.py
+++ saving_contacts.py
@@ -7,7 +7,7 @@ def main():
     db = sqlite3.connect("contacts.db")
     #db.execute("create table contacts(Name, Surname, Phone_number, Email_address)")
     
-    action = input("what are you here for?(add/view)").lower()
+    action = input("what are you here for?(add/view) :").lower()
     if action == 'add':
         while True:
             name= input("Enter name: ")
@@ -20,21 +20,30 @@ def main():
             more = input("Do you want to add more Y/N: ").lower()
             if more == "n": break
             
-        view = input("View the contacts?(Y/N)").lower()
+        view = input("View the contacts?(Y/N): ").lower()
         
-        if view == yes or view == y: 
+        if view == "yes" or view == "y": 
             table = db.execute("select*from contacts")
             for row in table:
                 print(row)
                 
-        
     else:
         
         table = db.execute("select*from contacts")
         for row in table:
             print(row)
                 
-       
+    dele = input("Do you what to delete a contact? (Y/N): ").lower()
+
+    if dele == 'yes' or dele == 'y':
+
+        del_cont = input("which contact do you want to delete?(Email_address/Name/Surname): ").Capitalize()
+        value = input("The value of the contact you want to delete?: ")
+
+        query = f"DELETE FROM contacts WHERE {del_cont} = ?"
+        db.execute(query, (value,))
+        db.commit()
+        print("Delete of contact complete!")
     db.close()
     
 main()
